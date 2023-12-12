6280530aaa6a52834e96f7bd20407c7476bc704d
CarlosI01 <91437492+CarlosI01@users.noreply.github.com>
2023-12-12 12:28:07 -0500

    first commit
    this is my first commit

---
+++ AdminSecurity/src/main/java/ec/com/cognoware/adminSecurity/AdminSecurityApplication.java
@@ -0,0 +1,13 @@
+package ec.com.cognoware.adminSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminSecurityApplication.class, args);
+	}
+
+}
